347329b35869890131dc50376190c2782684d6ac
abcei2 <santiago.salgado.duque@gmail.com>
2021-06-25 07:32:02 -0500

    It work when i recieve offer, create remotedescriptor, adding track and then generating an answer, not working on chrom

--- src/App.js
+++ src/App.js
@@ -1,116 +1,17 @@
 
 import './App.css';
-import React, { useState } from 'react';
 
-// import Video from 'react-native-video';
-var JanusClient = require('janus-videoroom-client').Janus;
-var client = new JanusClient({
-  url: 'ws://localhost:8188'
-});
-var pc = null;
-
-function negotiate(answer_json,offer_json) {
-  pc.addTransceiver('video', {direction: 'recvonly'});
-  return pc.createOffer().then(function(offer) {
-      console.log("offer",offer.sdp)
-      return pc.setLocalDescription(offer);
-  }).then(function() {
-      // wait for ICE gathering to complete
-      return new Promise(function(resolve) {
-          if (pc.iceGatheringState === 'complete') {
-              resolve();
-          } else {
-              function checkState() {
-                  if (pc.iceGatheringState === 'complete') {
-                      pc.removeEventListener('icegatheringstatechange', checkState);
-                      resolve();
-                  }
-              }
-              pc.addEventListener('icegatheringstatechange', checkState);
-          }
-      });
-  }).then(function() {
-      return pc.setRemoteDescription(offer_json);  
-  }).catch(function(e) {
-      alert(e);
-  });
-}
-var config = {
-  sdpSemantics: 'unified-plan'
-};
-
-if (true) {
-  config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
-}
-
-pc = new RTCPeerConnection(config);
-
-
-client.onConnected(()=>{
-  client.createSession().then((session)=>{
-    console.log("CREATING SESSION")
-    session.videoRoom().getFeeds("1234").then((feeds)=>{
-      console.log(feeds)
-      for(let feed of feeds) {
-          console.log(feed)
-          session.videoRoom().listenFeed(1234, feed).then((listenerHandle)=>{
-
-              var offerSdp = listenerHandle.getOffer();
-              console.log(offerSdp,listenerHandle);
-              session.videoRoom().publishFeed(1234, offerSdp).then((publisherHandle)=>{
-                var answerSdp = publisherHandle.getAnswer();
-                console.log(answerSdp)
-                
-                let answer_json={sdp:answerSdp,type:"answer"};
-                let offer_json={sdp:offerSdp,type:"offer"};
-                negotiate(answer_json,offer_json);
-              
-              });
-          });
-      }
-    });
-    
-    return session.videoRoom().defaultHandle();
-  }).then((videoRoomHandle)=>{
-    // videoRoomHandle.connect()
-  })
-  .catch((err)=>{
-    console.log("SOME ERROR",err)
-  })
-});
-
-client.onDisconnected(()=>{
-  console.log("DISCONNECTED")
-    
-});
-client.onError((err)=>{
-  console.log("DISCONNECTED")
-});
-pc.addEventListener('track', function(evt) {
-  console.log(evt.streams[0].getTracks(),document.getElementById('video'))
-  if (evt.track.kind === 'video') {
-    // console.log("asdasd")
-    document.getElementById('video').srcObject = evt.streams[0];
-    
-    console.log(evt,document.getElementById('video'))
-    // setVideoSrc("/example1.mp4")
-  } else {
-    document.getElementById('audio').srcObject = evt.streams[0];
-  }
-});
 function App() {
 
-  const [video_src, setVideoSrc] = useState("/example2.m4v");    
   
   // connect audio / video
 
 
  
-  client.connect();   
   return (
       <div id="media" height="400px" width="400px">
         <h2>Media</h2>
-        {video_src}
+
         <video id="video" autoPlay={true}>
         </video>
         
--- src/index.js
+++ src/index.js
@@ -5,7 +5,117 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
+var JanusClient = require('janus-videoroom-client').Janus;
+var client = new JanusClient({
+  url: 'ws://207.246.118.54:8188'
+});
+var pc = null;
 
+var config = {
+  sdpSemantics: 'unified-plan'
+};
+
+if (true) {
+  config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
+}
+
+pc = new RTCPeerConnection(config);
+
+
+client.onConnected(()=>{
+  client.createSession().then((session)=>{
+    console.log("CREATING SESSION")
+    session.videoRoom().getFeeds("1234").then((feeds)=>{
+      console.log(feeds)
+      for(let feed of feeds) {
+        console.log(feed)
+        session.videoRoom().listenFeed(1234, feed).then((listenerHandle)=>{
+
+          var offerSdp = listenerHandle.getOffer();
+          // console.log(offerSdp,listenerHandle);
+          
+          listenerHandle.trickle("candidate").then(()=>{
+            console.log("asdas")
+          });
+          let offer_json={sdp:offerSdp,type:"offer"};
+          pc.setRemoteDescription(offer_json);
+
+          pc.addTransceiver('video', {direction: 'recvonly'});
+          pc.createAnswer().then(function(answer) {
+            console.log(answer)
+            return pc.setLocalDescription(answer);
+          }).then(function() {
+            // wait for ICE gathering to complete
+            return new Promise(function(resolve) {
+                if (pc.iceGatheringState === 'complete') {
+                    resolve();
+                } else {
+                    function checkState() {
+                        if (pc.iceGatheringState === 'complete') {
+                            pc.removeEventListener('icegatheringstatechange', checkState);
+                            resolve();
+                        }
+                    }
+                    pc.addEventListener('icegatheringstatechange', checkState);
+                }
+            });
+          }).then(function() {
+            var answer = pc.localDescription;
+            listenerHandle.setRemoteAnswer(answer.sdp).then(()=>{
+              console.log("ANSWERD SEND",answer.sdp)              
+          
+            });
+            // Send the answer to the remote peer through the signaling server.
+          })
+          // negotiate(offer_json);
+          // session.videoRoom().publishFeed(1234, offerSdp).then((publisherHandle)=>{
+          //   var answerSdp = publisherHandle.getAnswer();
+          //   listenerHandle.setRemoteAnswer(answerSdp).then(()=>{
+          //     console.log("ANSWERD SEND")
+              
+          
+          //     let answer_json={sdp:answerSdp,type:"answer"};
+          //     let offer_json={sdp:offerSdp,type:"offer"};
+          //     negotiate(answer_json,offer_json);
+          //   });
+          //   publisherHandle.trickle("aiortc").then(()=>{
+          //     console.log("asdas")
+          //   });
+          
+            // });
+        });
+        break
+      }
+    });
+    
+    return session.videoRoom().defaultHandle();
+  }).then((videoRoomHandle)=>{
+    // console.log(videoRoomHandle.connect())
+  })
+  .catch((err)=>{
+    console.log("SOME ERROR",err)
+  })
+});
+
+client.onDisconnected(()=>{
+  console.log("DISCONNECTED")
+    
+});
+client.onError((err)=>{
+  console.log("DISCONNECTED")
+});
+pc.addEventListener('track', function(evt) {
+  console.log(evt.track.kind,evt.streams[0])
+  if (evt.track.kind === 'video') {
+    document.getElementById('video').srcObject = evt.streams[0];
+    
+  } else {
+    document.getElementById('audio').srcObject = evt.streams[0];
+  }
+});
+
+
+client.connect();   
 
 
 ReactDOM.render(
